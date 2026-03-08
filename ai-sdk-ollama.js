9b683cf3ac6e90141779783883a9d89403684e79
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-03-08 18:07:06 -0300

    Update AI SDK Ollama example file for generating Pop Rock musics

--- ai-sdk-ollama.js
+++ ai-sdk-ollama.js
@@ -7,11 +7,15 @@
 import { ollama } from 'ai-sdk-ollama';
 import { generateText } from 'ai';
 
+import fs from 'fs';
+
 // Works in both Node.js and browsers
 const { text } = await generateText({
   model: ollama('llama3.2:1b'),
-  prompt: 'Write a music in Spanish',
+  prompt: 'Write a Pop Rock romantic music, in Spanish',
   temperature: 0.8,
 });
 
 console.log(text);
+
+fs.writeFileSync(`music.${Date.now()}.txt`, text.trim())
